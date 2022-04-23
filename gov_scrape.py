585c59bc17c9da25b3da3d4094d724f6099f3943
Chengmou LEI <chengmou.lei@studenti.luiss.it>
2022-04-23 13:53:28 +0200

    now after scrape max number we can successfully reload the page and reselect, but I did not try if it works for the next page

--- gov_scrape.py
+++ gov_scrape.py
@@ -1,11 +1,6 @@
 from selenium import webdriver
-from pyquery import PyQuery as pq
-from selenium.webdriver.chrome.options import Options
 from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
-from selenium.webdriver.chrome.service import Service
-from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
 
 #load webpage
@@ -13,26 +8,26 @@ driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
 url='https://zfwzzc.www.gov.cn/check_web/databaseInfo/download#page-1'
 driver.get(url)
 
-'''
+
 #count how many li[x] in this page
 count = len(driver.find_elements_by_xpath("/html/body/div[4]/div[2]/ul//li/div")) #can count the total number at all pages
+max_group = int(count/5) #13 groups in total
 
+for j in range(max_group): #0-12
+    start_index = 1+5*j
+    end_index = start_index+4
 
-for i in range(1,count+1):
-    #click one department
-    xpath_dep= '/html/body/div[4]/div[2]/ul/li['+str(i)+']/div'
-    driver.find_element(by=By.XPATH, value=xpath_dep).click() #click to select dep
-    #click to download
-    xpath_download = '/html/body/div[4]/div[2]/div[1]/div[2]' #click download button
-    driver.find_element(by=By.XPATH, value=xpath_download).click()
-'''
+    for i in range(start_index,end_index+1):#start and end indicated
+        #click one department
+        xpath_dep= '/html/body/div[4]/div[2]/ul/li['+str(i)+']/div'
+        driver.find_element(by=By.XPATH, value=xpath_dep).click() #click to select dep
+        #click to download
+        xpath_download = '/html/body/div[4]/div[2]/div[1]/div[2]' #click download button
+        driver.find_element(by=By.XPATH, value=xpath_download).click()
+    
+    #reload the page to reclick department
+    driver.refresh()
 
-
-#cancel click of department
-xpath_dep= '/html/body/div[4]/div[2]/ul/li[1]/div'
-driver.find_element(by=By.XPATH, value=xpath_dep).click()
-xpath_cancel='/html/body/div[4]/div[2]/div[2]/ul/li[1]/div'
-driver.find_element(by=By.XPATH, value=xpath_cancel).click()
 
 
 
