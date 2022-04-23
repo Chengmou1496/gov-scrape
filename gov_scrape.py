2a018b7890536e526d666f7378dd7f39f5df14a5
Chengmou LEI <chengmou.lei@studenti.luiss.it>
2022-04-23 12:45:39 +0200

    can download the first 5, but then need to cancel all selected and reselect the next five to download

--- gov_scrape.py
+++ gov_scrape.py
@@ -13,13 +13,18 @@ driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
 url='https://zfwzzc.www.gov.cn/check_web/databaseInfo/download#page-1'
 driver.get(url)
 
-#click one department (five deps at a time)
-xpath_dep = '/html/body/div[4]/div[2]/ul/li[1]/div' #change li[1] 
-driver.find_element(by=By.XPATH, value=xpath_dep).click() #click to select dep
 
-#click to download
-xpath_download = '/html/body/div[4]/div[2]/div[1]/div[2]' #click download button
-driver.find_element(by=By.XPATH, value=xpath_download).click()
+#count how many li[x] in this page
+count = len(driver.find_elements_by_xpath("/html/body/div[4]/div[2]/ul//li/div")) #can count the total number at all pages
+
+
+for i in range(1,count+1):
+    #click one department
+    xpath_dep= '/html/body/div[4]/div[2]/ul/li['+str(i)+']/div'
+    driver.find_element(by=By.XPATH, value=xpath_dep).click() #click to select dep
+    #click to download
+    xpath_download = '/html/body/div[4]/div[2]/div[1]/div[2]' #click download button
+    driver.find_element(by=By.XPATH, value=xpath_download).click()
 
 
 
