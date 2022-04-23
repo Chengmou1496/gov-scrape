95fdbd14fd329369a474ba15d2d04b59ae99a09a
Chengmou LEI <chengmou.lei@studenti.luiss.it>
2022-04-23 11:08:28 +0200

    click the download button and download successfully, next step is to count how many similar elements and form a loop to download all files at this page

--- gov_scrape.py
+++ gov_scrape.py
@@ -1,20 +1,12 @@
 from selenium import webdriver
-import re
 from pyquery import PyQuery as pq
-import xlwt
-import xlrd
-import time
 from selenium.webdriver.chrome.options import Options
-import string
-import time
-import requests,random
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
 from webdriver_manager.firefox import GeckoDriverManager
-
 
 #load webpage
 driver = webdriver.Firefox(executable_path=GeckoDriverManager().install())
@@ -24,7 +16,10 @@ driver.get(url)
 #click one department (five deps at a time)
 xpath_dep = '/html/body/div[4]/div[2]/ul/li[1]/div' #change li[1] 
 driver.find_element(by=By.XPATH, value=xpath_dep).click() #click to select dep
-#error code:handshake failed; returned -1, SSL error code 1, net_error -101
+
+#click to download
+xpath_download = '/html/body/div[4]/div[2]/div[1]/div[2]' #click download button
+driver.find_element(by=By.XPATH, value=xpath_download).click()
 
 
 
